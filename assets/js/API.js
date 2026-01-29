c4596d0bf94d437be02f181f22b917996e216e5d
antonjensenv2-sudo <antonjensenv2@gmail.com>
2026-01-29 11:02:10 +0100

    made the right structure

---
+++ assets/js/API.js
@@ -0,0 +1,83 @@
+const ROOT = document.getElementById('root')
+
+export const getFilms = () => {
+
+    fetch('https://swapi.info/api/films')
+        .then(response => response.json())
+        .then(data => {
+            const ulWrapper = document.createElement('ul')
+
+            for (const item of data) {
+                const { title, episode_id, director, release_date } = item
+
+                const liWrapper = document.createElement('li')
+                liWrapper.innerText = title
+
+                const ulDetails = document.createElement('ul')
+
+                const liDir = document.createElement('li')
+                liDir.innerText = `Instruktør: ${director}`
+
+                const liEpisode = document.createElement('li')
+                liEpisode.innerText = `Episode: ${episode_id}`
+
+                const liRelease = document.createElement('li')
+                liRelease.innerText = `Udgivelsesår: ${new Date(release_date).getFullYear()}`
+
+                ulDetails.append(liDir, liEpisode, liRelease)
+
+                liWrapper.append(ulDetails)
+                
+                ulWrapper.append(liWrapper)
+
+                console.log(ulWrapper);
+
+            }
+            ROOT.innerHTML = '';
+            ROOT.append(ulWrapper)
+
+        })
+        .catch(error => {
+            console.error(error)
+        })
+}
+
+export const getPeople = () => {
+
+    fetch('https://swapi.info/api/people')
+        .then(response => response.json())
+        .then(data => {
+            const ulWrapper = document.createElement('ul')
+            ulWrapper.className = 'people'
+
+            for (const item of data.slice(0,10)) {
+                const { name, gender, films } = item
+
+                const liWrapper = document.createElement('li')
+                liWrapper.innerHTML = `<b>${name}</b>`
+
+                const ulDetails = document.createElement('ul')
+
+                const liDir = document.createElement('li')
+                liDir.innerText = `Køn: ${gender}`
+
+                const liEpisode = document.createElement('li')
+                liEpisode.innerHTML = `Film: <ul><li>${films.join('</li><li>')}</li></ul>`
+
+                ulDetails.append(liDir, liEpisode)
+
+                liWrapper.append(ulDetails)
+                
+                ulWrapper.append(liWrapper)
+
+                console.log(ulWrapper);
+
+            }
+            ROOT.innerHTML = '';
+            ROOT.append(ulWrapper)
+
+        })
+        .catch(error => {
+            console.error(error)
+        })
+}
